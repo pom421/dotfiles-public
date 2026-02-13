aa50c4f8672da390fc0ddc8abfe55421ab861800
Pierre-Olivier Mauguet <pierre-olivier.mauguet@dgfip.finances.gouv.fr>
2026-02-13 12:17:12 +0100

    nvim

---
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    -- chargement du thème
+    vim.cmd([[colorscheme tokyonight]])
+  end,
+}
+
